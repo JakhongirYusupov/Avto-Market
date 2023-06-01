e181a30f81d780fb56ad49a361e2f356ca6d7490
Jakhongir <jakhongiryusupov58@gmail.com>
2023-06-01 18:13:15 +0500

    detail some codes writed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import CustomBtn from "../CustomBtn/CustomBtn";
 import "./Header.scss";
 import { RxPerson } from "react-icons/rx";
+import { FaRegHeart } from "react-icons/fa";
+import { GrCart } from "react-icons/gr";
 
 export default function Header() {
   return (
     <div className="header">
+      <div className="header-lovely scaleAnimation">
+        <FaRegHeart />
+        <div className="header-lovely-dot"></div>
+      </div>
+      <div className="header-cart scaleAnimation">
+        <div className="header-cart-count ">15</div>
+        <GrCart />
+      </div>
       <CustomBtn title={"Profile"} bold={true} icon={<RxPerson />} />
     </div>
   );
--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,12 +1,75 @@
+import { useLocation } from "react-router-dom";
 import Header from "../../components/Header/Header";
+import HeaderHero from "../../components/HeaderHero/HeaderHero";
+import image from "../../assets/images/maluba.png";
 import "./Detail.scss";
 
 export default function Detail() {
+  const { search } = useLocation();
+  const links = [
+    { title: "Modellari", url: "/" },
+    {
+      title: "Chevrolet turlari",
+      url: `/model-types/${search.split("&")[0]}`,
+    },
+    { title: "Chevrolet Malibu" },
+  ];
   return (
-    <div>
+    <main className="detail">
       <div className="container">
         <Header />
+        <HeaderHero links={links} title={"Chevrolet Malibu"} />
+        <section className="detail-wrapper">
+          <div className="detail-info">
+            <div className="detail-info-hero">
+              <h2>Chevrolet Malibu</h2>
+              <p>329 900 000 so‘m dan</p>
+              <img src={image} alt="carimage" />
+            </div>
+            <div className="detail-info-desc">
+              <div className="detail-info-desc-item">
+                <strong>Marka:</strong>
+                <span>CHEVROLeT</span>
+              </div>
+              <div className="detail-info-desc-item">
+                <strong>Tanirovkasi:</strong>
+                <span>Yo‘q</span>
+              </div>
+              <div className="detail-info-desc-item">
+                <strong>Motor:</strong>
+                <span>1.6</span>
+              </div>
+              <div className="detail-info-desc-item">
+                <strong>Year:</strong>
+                <span>2016</span>
+              </div>
+              <div className="detail-info-desc-item">
+                <strong>Color:</strong>
+                <span>Oq</span>
+              </div>
+              <div className="detail-info-desc-item">
+                <strong>Distance:</strong>
+                <span>3000 km</span>
+              </div>
+              <div className="detail-info-desc-item">
+                <strong>Gearbook:</strong>
+                <span>Avtomat karobka</span>
+              </div>
+              <div className="detail-info-desc-item">
+                <p className="detail-info-desc-item-text">
+                  <strong>Deseription:</strong> Mishina ideal holatda krasska
+                  top toza 100tali. Ayol kishiniki judayam akuratno haydalgan.
+                </p>
+              </div>
+              <div className="detail-info-desc-item-border"></div>
+              <div className="detail-info-desc-item detail-info-desc-item-cost">
+                <strong>Umumiy xarajat:</strong>
+                <span>329 900 000 so'm dan</span>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    </div>
+    </main>
   );
 }
